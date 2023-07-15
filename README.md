ff14ba70d676cd1e177cb9413ab3172ff8a36cce
Ahmed Khanzada <me@enzu.ru>
2023-07-15 17:01:39 -0400

    Rename

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ivy completion for Geiser symbols
+
+This is a simple package that provides [Ivy](https://github.com/abo-abo/swiper) completion for Geiser symbols, providing a definition of the symbol using Geiser.
+
+## Installation
+
+```
+(straight-use-package
+ '(ivy-guile
+   :type git
+   :host github
+   :repo "enzuru/ivy-geiser"))
+```
+
+## License
+
+Licensed under the GPLv3; copyright is assigned to my eponymous nonprofit [enzu.ru](https://enzu.ru)
--- ivy-geiser.el
+++ ivy-geiser.el
@@ -1,4 +1,4 @@
-;;; ivy-guile.el --- Ivy support for Guile symbols
+;;; ivy-geiser.el --- Ivy support for Geiser symbols
 
 ;; Copyright (C) enzu.ru
 ;; SPDX-License-Identifier: GPL-3.0
@@ -16,15 +16,15 @@
          (response-list-cleaned (mapcar #'ivy-geiser-get-symbol response-list)))
     response-list-cleaned))
 
-(defun ivy-guile-describe-symbol ()
-  "Describe any Guile symbol"
+(defun ivy-geiser-describe-symbol ()
+  "Describe any Geiser symbol"
   (interactive)
-  (ivy-read "Describe Guile symbol: "
+  (ivy-read "Describe Geiser symbol: "
             (ivy-geiser-completion)
-            :history 'ivy-describe-guile-symbol-history
+            :history 'ivy-describe-geiser-symbol-history
             :require-match t
             :action (lambda (symbol-string)
                       (geiser-doc-symbol (make-symbol symbol-string)))
             :caller 'ivy-describe-common-lisp--spec-symbol))
 
-(provide 'ivy-guile)
+(provide 'ivy-geiser)

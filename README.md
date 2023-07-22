8ade7175feb87142c1fcfe8ab5016448c53725c1
Ahmed Khanzada <me@enzu.ru>
2023-07-22 11:34:23 -0400

    Switch to completing-read

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Ivy completion for Geiser symbols
+# completing-read support for Geiser
 
-This is a simple package that provides [Ivy](https://github.com/abo-abo/swiper) completion for Geiser symbols, providing a definition of the symbol using Geiser.
+This is a simple package that provides compelting-read for Geiser symbols. When a symbol is select, a definition of the symbol using Geiser.
+
+Use the completion framework of your choice, whether (ivy)[https://github.com/abo-abo/swiper], (vertico)[https://github.com/minad/vertico], (mct)[https://git.sr.ht/~protesilaos/mct], or just the standard icomplete.
 
 I have only tested this with Guile, as that is the only Scheme distribution that I use.
 
@@ -8,12 +10,22 @@ I run the completion query using `(apropos ".*")`. If I need to do something els
 
 ## Installation
 
-```
+```lisp
 (straight-use-package
- '(ivy-geiser
+ '(completing-read-geiser
    :type git
    :host github
-   :repo "enzuru/ivy-geiser"))
+   :repo "enzuru/completing-read-geiser"))
+
+(define-key scheme-mode-map (kbd "C-h s") 'completing-read-geiser-describe-symbol)
+(define-key geiser-repl-mode-map (kbd "C-h s") 'completing-read-geiser-describe-symbol)
+(require 'completing-read-geiser)
+```
+
+## Using with Ivy
+
+```lisp
+(setf completing-read-function #'ivy-completing-read)
 ```
 
 ## License
---
+++ completing-read-geiser.el
@@ -0,0 +1,41 @@
+;;; completing-read-geiser.el --- Search Geiser symbols with completing-read  -*- lexical-binding:t; coding:utf-8 -*-
+
+;; Copyright (C) enzu.ru
+
+;; Homepage: https://github.com/enzuru/completing-read-geiser
+;; Keywords: lisp matching tools
+
+;; Package-Version: 1.0.0
+;; Package-Requires: ((emacs "24.4") (geiser "0.28.2"))
+
+;; SPDX-License-Identifier: GPL-3.0
+
+;;; Commentary:
+
+;; completing-read provides a list of symbols available in your Geiser session
+
+;;; Code:
+
+(require 'geiser)
+
+(defun completing-read-geiser--get-symbol (unparsed-string)
+  "Grab symbol from UNPARSED-STRING."
+  (string-match "\\(?:^\\|[^:]:\\)[[:space:]]+\\([^[:space:]]+\\)" unparsed-string)
+  (match-string 1 unparsed-string))
+
+(defun completing-read-geiser-search ()
+  "Send apropos command to Geiser."
+  (let* ((response-alist (geiser-eval--send/wait `(apropos ".*")))
+         (response-string (cdr (assoc 'output response-alist)))
+         (response-list (butlast (split-string response-string "\n")))
+         (response-list-parsed (mapcar #'completing-read-geiser--get-symbol response-list)))
+    response-list-parsed))
+
+(defun completing-read-geiser ()
+  "Get list of symbols available in your Geiser session"
+  (interactive)
+  (geiser-doc-symbol (make-symbol (completing-read "Look up symbol in Geiser" (completing-read-geiser-search)))))
+
+(provide 'completing-read-geiser)
+
+;;; completing-read-geiser.el
